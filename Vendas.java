257d5af98ca862f9f63a267aa4b18740d8b90b03
beatrizcristiny <beatrizcrispires@gmail.com>
2026-03-14 15:34:52 -0300

    innstalando o javac

--- Vendas.java
+++ Vendas.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
 
 public class Vendas{
-    
+    public static void main(String[] args){
+        Scanner scanner = new scanner(System.in);
+        double custo
+        double precov
+        System.out.println("Digite o preço de custo do produto")
+        custo = scanner.nestDouble();
+        preco = custo * 1.25;
+        System.out.println("O preço final de venda é: ", +preco);
+
+    }
 }
