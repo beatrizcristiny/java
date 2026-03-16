402a25df228663df57cbe015bfc34964405cf1b1
beatrizcristiny <beatrizcrispires@gmail.com>
2026-03-16 19:13:35 -0300

    ex2

--- Ex2.java
+++ Ex2.java
@@ -1,7 +1,16 @@
 import java.util.Scanner;
 public class Ex2{
     public static void main(String [] args){
-    Scanner scanner = new scanner(System.in)
+    Scanner scanner = new Scanner(System.in);
+    Double n;
+    Double d;
+    System.out.println("Digite um número");
+    n = scanner.nextDouble();
+    System.out.println("Digite outro número");
+    d = scanner.nextDouble();
+    
+    Double resultado = (n > d)? n : d;
+    System.out.println("O maior número é " +resultado);
 
     }
     
