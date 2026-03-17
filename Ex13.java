8350dcfee9ec42df72b60a9320f07e7584f7b5db
beatrizcristiny <beatrizcrispires@gmail.com>
2026-03-17 08:48:36 -0300

    Ex13

--- Ex13.java
+++ Ex13.java
@@ -7,40 +7,40 @@ public class Ex13{
         mes = scanner.nextInt();
         switch (mes){
             case 1:
-                System.out.println("Verão");
+                System.out.println(" Janeiro- Verão");
                 break;
             case 2:
-                System.out.println("Verão");
+                System.out.println("Fevereiro- Verão");
                 break;
             case 3:
-                System.out.println("Verão");
+                System.out.println(" Março- Verão / Outono");
                 break;
             case 4:
-                System.out.println("Outono");
+                System.out.println("Abril- Outono");
                 break;
             case 5:
-                System.out.println("Outono");
+                System.out.println("Maio- Outono");
                 break;
             case 6:
-                System.out.println("Outono");
+                System.out.println("Junho- Outono / Inverno");
                 break;
             case 7:
-                System.out.println("Inverno");
+                System.out.println("Julho- Inverno");
                 break;
             case 8:
-                System.out.println("Inverno");
+                System.out.println("Agosto- Inverno");
                 break;
             case 9:
-                System.out.println("Inverno");
+                System.out.println("Setembro- Inverno / Primavera");
                 break;
             case 10:
-                System.out.println("Primavera");
+                System.out.println("Outubro- Primavera");
                 break;
             case 11:
-                System.out.println("Primavera");
+                System.out.println("Novembro- Primavera");
                 break;
             case 12:
-                System.out.println("Primavera/ verão");
+                System.out.println("Dezembro- Primavera/ verão");
                 break;  
         }
     }
